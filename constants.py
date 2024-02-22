89c494c56cbf2cda456e0f72806fa4a4bcfb8bc6
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-21 20:55:55 -0600

    added constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+# the default width and height of the window
+SCREEN_WIDTH = 840
+SCREEN_HEIGHT = 720
+
+# the minimum width and height of the window
+MIN_WIDTH = 420
+MIN_HEIGHT = 360
+
+BLUE = (121, 156, 178)
+WHITE = (212, 223, 229)
+PINK = (255, 145, 220, 100)
+
+FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
