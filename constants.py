f4a63ada3b5485bb2f4eeb897c0c08f6fd00f0ff
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-22 20:00:47 -0600

    added offsets for pieces

--- constants.py
+++ constants.py
@@ -11,5 +11,20 @@ BLUE = (121, 156, 178)
 WHITE = (212, 223, 229)
 PINK = (255, 145, 220, 150)
 
+# all the moves the knight can make
+KNIGHT_OFFSETS = (
+    (2, 1),
+    (2, -1),
+    (-2, 1),
+    (-2, -1),
+    (1, 2),
+    (1, -2),
+    (-1, 2),
+    (-1, -2),
+)
+STRAIGHT_OFFSETS = (-8, -1, 1, 8)
+DIAGONAL_OFFSETS = (-9, -7, 7, 9)
+
 # starting position fen
-FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+# FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+FEN = "r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1"
