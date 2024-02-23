eed8cbdd210aafaf563eca83ff9f647c521a14f6
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-23 14:46:29 -0600

    added evaluate function (unused for now)

--- objects/engine.py
+++ objects/engine.py
@@ -1,6 +1,7 @@
 import random
 from objects.board import Board
 from objects.move import Move
+from constants import PIECE_VALUES
 
 
 class Engine:
@@ -9,3 +10,19 @@ class Engine:
 
     def search(self, board: Board) -> Move:
         return random.choice(board.get_moves())
+
+    def evaluate(self, board: Board) -> int:
+        score = 0
+
+        for rank in range(8):
+            for file in range(8):
+                piece = board.board[rank][file]
+
+                # if the piece is white add the value, otherwise subtract the value
+                if piece.upper() in PIECE_VALUES:
+                    if piece.isupper():
+                        score += PIECE_VALUES[piece.upper()]
+                    else:
+                        score -= PIECE_VALUES[piece.upper()]
+
+        return score
