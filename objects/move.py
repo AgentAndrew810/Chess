5f774c4a1e1f63ddea26c496e768d79900cc127d
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-22 23:25:11 -0600

    changed move class to store rank and files as opposed to pos

--- objects/move.py
+++ objects/move.py
@@ -1,4 +1,16 @@
 class Move:
-    def __init__(self, old_pos: int, new_pos: int) -> None:
-        self.old_pos = old_pos
-        self.new_pos = new_pos
+    def __init__(
+        self, old_rank: int, old_file: int, new_rank: int, new_file: int
+    ) -> None:
+        self.old_rank = old_rank
+        self.old_file = old_file
+        self.new_rank = new_rank
+        self.new_file = new_file
+
+    @property
+    def old_pos(self) -> None:
+        return (self.old_rank, self.old_file)
+
+    @property
+    def new_pos(self) -> None:
+        return (self.new_rank, self.new_file)
