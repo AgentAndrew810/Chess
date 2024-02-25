54d5b035252e4613caf643f54854d35a51888c32
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-24 18:53:12 -0600

    added castling_type flag

--- objects/move.py
+++ objects/move.py
@@ -1,11 +1,18 @@
 class Move:
     def __init__(
-        self, old_rank: int, old_file: int, new_rank: int, new_file: int
+        self,
+        old_rank: int,
+        old_file: int,
+        new_rank: int,
+        new_file: int,
+        castling_type: str | None = None,
     ) -> None:
         self.old_rank = old_rank
         self.old_file = old_file
         self.new_rank = new_rank
         self.new_file = new_file
+
+        self.castling_type = castling_type
 
     @property
     def old_pos(self) -> tuple[int, int]:
